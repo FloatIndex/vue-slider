eb4b0cb17d7c389d4de9d525706f26cfd3c8af69
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-02-16 21:17:20 +0100

    final commit

--- main.js
+++ main.js
@@ -1,95 +1,51 @@
-const items = [
+const app = new Vue (
     {
-        photo: 'img/01.jpg',
-        title: "Svezia",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
-    },
-    {
-        photo: 'img/02.jpg',
-        title: "Svizzera",
-        text: "Lorem ipsum."
-    },
-    {
-        photo: 'img/03.jpg',
-        title: "Gran Bretagna",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        photo: 'img/04.jpg',
-        title: "Germania",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
-    },
-    {
-        photo: 'img/05.jpg',
-        title: "Paradise",
-        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+        el: '#app',
+        data: {
+            active: 0,
+            items: [
+                {
+                    photo: 'img/01.jpg',
+                    title: "Svezia",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+                },
+                {
+                    photo: 'img/02.jpg',
+                    title: "Svizzera",
+                    text: "Lorem ipsum."
+                },
+                {
+                    photo: 'img/03.jpg',
+                    title: "Gran Bretagna",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+                },
+                {
+                    photo: 'img/04.jpg',
+                    title: "Germania",
+                    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+                },
+                {
+                    photo: 'img/05.jpg',
+                    title: "Paradise",
+                    text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+                }
+            ]
+        },
+        methods: {
+            prevActive() {
+                if(this.active == 0) {
+                    this.active = this.items.length - 1;
+                } else {
+                    this.active--;
+                }
+            },
+            nextActive() {
+                if(this.active == this.items.length - 1) {
+                    this.active = 0;
+                } else {
+                    this.active++;
+                }
+            }
+        }
     }
-];
-
-const itemsRef = document.getElementsByClassName('items')[0];
-const thumbsRef = document.getElementsByClassName('thumbs')[0];
-let item = '';
-let thumb = '';
-let active = 1;
-
-for (let key in items) {
-    item += `
-        <div class="item">
-            <img src="${items[key].photo}" alt="">
-            <div class="text">
-                <h3>${items[key].title}</h3>
-                <p>${items[key].text}</p>
-            </div>
-        </div>`
-    thumb += `
-        <div class="thumb">
-            <img src="${items[key].photo}" alt="">
-        </div>
-    `
-}
-
-itemsRef.innerHTML = item;
-document.getElementsByClassName('item')[active].classList.add('active');
-
-thumbsRef.innerHTML += thumb;
-document.getElementsByClassName('thumb')[active].classList.add('active');
-
-const prev = document.querySelector('.prev');
-prev.addEventListener('click', function() {
-    if(active == 0) {                                                                   
-        active = items.length - 1; 
-
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active < items.length) {
-        --active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } 
-});
-
-const next = document.querySelector('.next');
-next.addEventListener('click', function() {
-    if(active < items.length - 1) {
-        ++active
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    } else if(active == items.length - 1) { 
-        active = 0;
-        document.querySelector('.item.active').classList.remove('active');
-        document.getElementsByClassName('item')[active].classList.add('active');
-
-        document.querySelector('.thumb.active').classList.remove('active');
-        document.getElementsByClassName('thumb')[active].classList.add('active');
-    }
-});
-
+);
